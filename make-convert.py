ffe3b5f9004c690ca5088dc16bd304a2e9f93458
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2021-11-13 10:58:07 -0800

    Removed need for grep

--- make-convert.py
+++ make-convert.py
@@ -4,8 +4,14 @@ import anitopy
 anitopy_options = {'allowed_delimiters': ' '}
 
 # Get a list of all the mkv video files
+'''
 dir_grep = sp.run("dir /b | grep .mkv", stdout=sp.PIPE, shell=True, universal_newlines=True)
 videos = dir_grep.stdout.strip().split('\n')
+'''
+dir = sp.run("dir /b", stdout=sp.PIPE, shell=True, universal_newlines=True)
+videos = dir.stdout.strip().split('\n')
+#print(videos)
+videos = [v for v in videos if ".mkv" in v]
 #print(videos)
 
 script = open("convert.bat", "w")
