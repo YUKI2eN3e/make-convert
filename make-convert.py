caf60c6de5ec85cf7fe6a409ff8832c9fc1c7fd1
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2021-11-13 11:09:19 -0800

    Cleanup code, and catch missing episode number

--- make-convert.py
+++ make-convert.py
@@ -4,24 +4,24 @@ import anitopy
 anitopy_options = {'allowed_delimiters': ' '}
 
 # Get a list of all the mkv video files
-'''
-dir_grep = sp.run("dir /b | grep .mkv", stdout=sp.PIPE, shell=True, universal_newlines=True)
-videos = dir_grep.stdout.strip().split('\n')
-'''
 dir = sp.run("dir /b", stdout=sp.PIPE, shell=True, universal_newlines=True)
 videos = dir.stdout.strip().split('\n')
-#print(videos)
 videos = [v for v in videos if ".mkv" in v]
-#print(videos)
 
 script = open("convert.bat", "w")
 for v in videos:
 	vid = v.strip()
 	info = anitopy.parse(vid, options=anitopy_options)
-	e = info["episode_number"]
+	try:
+		e = info["episode_number"]
+	except KeyError:
+		e = ""
 	title = info["anime_title"]
 	title = title.replace(".", " ")
-	output = title + " - " + e + ".mp4"
+	if e != "":
+		output = title + " - " + e + ".mp4"
+	else:
+		output = title + ".mp4"
 	sub = vid.replace("[", "\\[")
 	sub = sub.replace("]", "\\]")
 	sub = sub.replace(",", "\\,")
